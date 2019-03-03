6147cd15eef318f62740461faea29c6468264df8
CarlW <carl@oskarroger.net>
2019-03-03 17:51:50 +0800

    Working on the search field component, geti. images from API while type

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import TextField from "material-ui/TextField";
 import SelectField from "material-ui/SelectField";
 import MenuItem from "material-ui/MenuItem";
+import axios from "axios";
 
 class Search extends Component {
   state = {
@@ -10,6 +11,22 @@ class Search extends Component {
     apiUrl: "https://pixabay.com/api",
     apiKey: "11770821-f454726aff5b615461e575d05",
     images: []
+  };
+
+  onTextChange = e => {
+    this.setState({ [e.target.name]: e.target.value }, () => {
+      axios
+        .get(
+          `${this.state.apiUrl}/
+        key=${this.state.apiKey}
+        &q=${this.state.searchText}
+        &image_type=photo
+        &per_page=${this.state.amount}
+        &safesearch=true`
+        )
+        .then(res => this.setState({ images: res.data.hits }))
+        .catch(err => console.log(err));
+    });
   };
 
   render() {
@@ -22,6 +39,19 @@ class Search extends Component {
           floatingLabelText="Search for Images"
           fullWidth={true}
         />
+        <br />
+        <SelectField
+          name="amount"
+          floatingLabelText="Amount"
+          value={this.state.amount}
+          onChange={this.onAmountChange}
+        >
+          <MenuItem value={5} primaryText="5" />
+          <MenuItem value={10} primaryText="10" />
+          <MenuItem value={15} primaryText="15" />
+          <MenuItem value={30} primaryText="30" />
+          <MenuItem value={50} primaryText="50" />
+        </SelectField>
       </div>
     );
   }
